c1c42997f28175c3f03150efb6a1bcf7a166be16
avl-vins <avl.vinss@gmail.com>
2024-11-05 22:09:52 -0500

    crea la carpeta  componnet y el archivo index que contiene los componentes genericos.

--- src/Hooks.jsx
+++ src/Hooks.jsx
@@ -1,7 +1,14 @@
+import { StParrafo, StSection } from "./components"
 
 export const Hooks = () => {
   return (
-    <h1>Hooks</h1>
-      
+    <StSection>
+        <h1>Hooks</h1>
+        <StParrafo>
+        Los hooks en React son funciones que permiten manipular el estado y el ciclo de vida de los componentes funcionales. 
+        Existen diferentes tipos de hooks: useState, useEffect, useContext, useMemo, useReducer, custom Hooks.</StParrafo>
+        <StParrafo>
+        </StParrafo>
+    </StSection>    
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+
+
+export const StSection = styled.section`
+    height:100vh;
+    padding: 2rem;
+    display:flex;
+    flex-direction:column;
+    align-items:center;
+` 
+export const StParrafo = styled.p`
+padding:1rem;
+// text-align:center;
+width:70%;
+`
